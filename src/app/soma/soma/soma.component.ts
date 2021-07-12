36d43d81ce5beaa6781ca5ac99122485ded36d73
Daniel Versoza Alves <daniel.ve3@gmail.com>
2021-07-11 23:16:13 -0300

    Finished Projeto Soma

--- src/app/soma/soma/soma.component.ts
+++ src/app/soma/soma/soma.component.ts
@@ -4,18 +4,18 @@ import { SomarService } from '../services';
 @Component({
   selector: 'app-soma',
   templateUrl: './soma.component.html',
-  styleUrls: ['./soma.component.css']
+  styleUrls: ['./soma.component.css'],
 })
 export class SomaComponent implements OnInit {
   private res!: number;
 
-  constructor(private somarService: SomarService) { }
+  constructor(private somarService: SomarService) {}
 
   somar(numero1: string, numero2: string): void {
     let n1: number;
     let n2: number;
-    n1 = parseFloat(numero1)
-    n2 = parseFloat(numero2)
+    n1 = parseFloat(numero1);
+    n2 = parseFloat(numero2);
     this.res = this.somarService.somar(n1, n2);
   }
 
@@ -23,7 +23,5 @@ export class SomaComponent implements OnInit {
     return this.res.toString();
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
